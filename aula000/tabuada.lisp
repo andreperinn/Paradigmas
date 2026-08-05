525a64f47fc9eeea6433cbc2f7d25647031fac27
André Perin Geraldo <88738089+andreperinn@users.noreply.github.com>
2026-08-05 20:11:22 -0300

    Delete salary and language info comments in tabuada.lisp
    Removed comments about Lisp salaries and language characteristics.

--- aula000/tabuada.lisp
+++ aula000/tabuada.lisp
@@ -19,14 +19,3 @@
       (return))))
 
 
-#|
-O salário médio para desenvolvedores que usam a linguagem Lisp ou seus dialetos gira em torno de US$ 80.000 a US$ 95.000 por ano no exterior. No Brasil, 
-o uso específico de Lisp é raro e focado em nichos acadêmicos ou de IA, 
-onde a remuneração depende do cargo geral de desenvolvimento (R$ 4.000 a R$ 16.000 mensais).
-|#
-
-#|
-A família de linguagens Lisp é essencialmente multiparadigma. Embora tenha nascido historicamente ligada ao 
-paradigma funcional (baseada no cálculo lambda e processamento de listas), os seus principais dialetos modernos 
-(como o Common Lisp) incorporam e combinam diferentes formas de programar
-|*
