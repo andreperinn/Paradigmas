81e31d6fcdb2cdcb4f39c4f2f8fc36123bc049dd
André Perin Geraldo <88738089+andreperinn@users.noreply.github.com>
2026-08-05 20:02:15 -0300

    Add multiplication table program in tabuada.lisp
    This program prompts the user for a number and prints its multiplication table from 1 to 10.

---
+++ aula000/tabuada.lisp
@@ -0,0 +1,19 @@
+(format t "Hello, World!~%")
+
+(format t "Digite o numero da tabuada: ")
+(finish-output)
+
+(let ((num (parse-integer (read-line)))
+      (numtabuada 1))
+
+  (loop
+    (let ((result (* num numtabuada)))
+      (format t "~d x ~d = ~d~%"
+              num
+              numtabuada
+              result))
+
+    (incf numtabuada)
+
+    (when (> numtabuada 10)
+      (return))))
